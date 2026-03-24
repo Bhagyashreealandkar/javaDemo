6dd683838c64bc8ff68f1b51439d65509c300b6b
Bhagyashreealandkar <32740655+Bhagyashreealandkar@users.noreply.github.com>
2026-03-24 22:57:52 +0530

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int a = 5;
+//		System.out.println(+a);   // 5
+//		System.out.println(-a);   // -5
+		System.out.println(a++);  // 5 (then a becomes 6)
+		System.out.println(++a);  // 7 (a becomes 7, then prints 7)
+		System.out.println(a--);  // 7 (then a becomes 6)
+		System.out.println(--a);  // 5
+
+
+	}
+
+}
